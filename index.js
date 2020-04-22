c285102e845c81d5f457c8842d59235aaa5b924a
khach2010 <41897918+khach2010@users.noreply.github.com>
2020-04-22 11:28:39 +0200

    create sign up form

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hi there for the second times");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(
+    `<div>
+    <form method="POST">
+      <input name="email" placeholder="email"></input>
+      <input name="password" placeholder="password"></input>
+      <input name="passwordConfirm" placeholder="password confirm"></input>
+      <button>Sign Up</button>
+    </form>
+  </div>`
+  );
+});
+
+app.post("/", (req, res) => {
+  res.send("Account created");
+});
+
+app.listen(3000, () => {
+  console.log("Port 3000 is running");
+});
