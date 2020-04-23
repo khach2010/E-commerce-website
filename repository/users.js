9d4aff06813e8b06cf62f47358628ac854ecbc75
khach2010 <41897918+khach2010@users.noreply.github.com>
2020-04-23 10:49:16 +0200

    delete user of a given id

--- repository/users.js
+++ repository/users.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
 class UsersRepository {
   constructor(filename) {
@@ -23,20 +24,38 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    attrs.id = this.randomID();
     const records = await this.getAll();
     records.push(attrs);
-    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    await this.writeAll(records);
+  }
+
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomID() {
+    return crypto.randomBytes(4).toString("hex");
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find(record => record.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter(record => record.id !== id);
+    await this.writeAll(filteredRecords);
   }
 }
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
-  await repo.create({
-    email: "test1@test1.com",
-    password: "password"
-  });
-  const users = await repo.getAll();
-  console.log(users);
+  await repo.delete("4163886a");
 };
 
 test();
