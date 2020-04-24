005acc2fcf8cf204c8606595aab618b38424aae2
khach2010 <41897918+khach2010@users.noreply.github.com>
2020-04-24 08:56:58 +0200

    install cookie session and store ID of that user inside the user cookie

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
 const usersRepo = require("./repository/users");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ["iloveyou"]
+  })
+);
 
-app.get("/", (req, res) => {
+app.get("/signup", (req, res) => {
   res.send(
-    `<div>
-    <form method="POST">
-      <input name="email" placeholder="email"></input>
-      <input name="password" placeholder="password"></input>
-      <input name="passwordConfirm" placeholder="password confirm"></input>
-      <button>Sign Up</button>
-    </form>
-  </div>`
+    `<p>Your ID is:${req.session.userID}</p>
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email"></input>
+        <input name="password" placeholder="password"></input>
+        <input name="passwordConfirmation" placeholder="password confirm"></input>
+        <button>Sign Up</button>
+      </form>
+    </div>`
   );
 });
 
-app.post("/", async (req, res) => {
+app.post("/signup", async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
   const existingUser = await usersRepo.getOneBy({ email });
   if (existingUser) {
@@ -27,8 +34,18 @@ app.post("/", async (req, res) => {
   if (password !== passwordConfirmation) {
     return res.send("password must match");
   }
+  //Create a user in our user repo represent this person
+  const user = await usersRepo.create({ email, password });
+
+  //Store the id of that user inside the users cookie
+  req.session.userID = user.id;
 
   res.send("Account created");
+});
+
+app.get("/signout", (req, res) => {
+  req.session = null;
+  res.send("You are logged out!");
 });
 
 app.listen(3000, () => {
