49a1b6083c1df56630ef87e61cac98a6c2a73258
khach2010 <41897918+khach2010@users.noreply.github.com>
2020-04-24 10:40:01 +0200

    create compare password  hash password . salt

--- index.js
+++ index.js
@@ -42,10 +42,35 @@ app.post("/signup", async (req, res) => {
 
   res.send("Account created");
 });
-
+// ------ SIGN OUT route ---------
 app.get("/signout", (req, res) => {
   req.session = null;
   res.send("You are logged out!");
+});
+
+//----- SIGN IN route -----------
+app.get("/signin", (req, res) => {
+  res.send(
+    `<div>
+      <form method="POST">
+        <input name="email" placeholder="email"></input>
+        <input name="password" placeholder="password"></input>
+        <button>Sign In</button>
+      </form>
+    </div>`
+  );
+});
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await usersRepo.getOneBy({ email });
+  if (!user) {
+    return res.send("Email not found");
+  }
+  if (user.password !== password) {
+    return res.send("invalid password");
+  }
+  req.session.userID = user.id;
+  res.send("You are signed in");
 });
 
 app.listen(3000, () => {
