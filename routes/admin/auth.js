7f85bdc2b291597f93175f1d313447b7a4e6a51f
khach2010 <41897918+khach2010@users.noreply.github.com>
2020-04-25 08:13:34 +0200

    displaying error messages to signin form

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -3,6 +3,11 @@ const { check, validationResult } = require("express-validator");
 const usersRepo = require("../../repository/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
+const {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation
+} = require("./validator");
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
@@ -11,41 +16,16 @@ router.get("/signup", (req, res) => {
 
 router.post(
   "/signup",
-  [
-    check("email")
-      .trim()
-      .normalizeEmail()
-      .isEmail()
-      .custom(async email => {
-        const existingUser = await usersRepo.getOneBy({ email });
-        if (existingUser) {
-          throw new Error("Email in use");
-        }
-      }),
-    check("password")
-      .trim()
-      .isLength({ min: 4, max: 20 }),
-    check("passwordConfirmation")
-      .trim()
-      .isLength({ min: 4, max: 20 })
-      .custom((passwordConfirmation, { req }) => {
-        if (passwordConfirmation !== req.body.password) {
-          throw new Error("Passwords must mactch");
-        }
-      })
-  ],
+  [requireEmail, requirePassword, requirePasswordConfirmation],
   async (req, res) => {
     const errors = validationResult(req);
-    console.log(errors);
+    if (!errors.isEmpty()) {
+      return res.send(signupTemplate({ req, errors }));
+    }
 
     const { email, password, passwordConfirmation } = req.body;
-
-    if (password !== passwordConfirmation) {
-      return res.send("password must match");
-    }
     //Create a user in our user repo represent this person
     const user = await usersRepo.create({ email, password });
-
     //Store the id of that user inside the users cookie
     req.session.userID = user.id;
 
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,24 @@
 const layout = require("../layout");
 
-module.exports = ({ req }) => {
+const getErrors = (errors, prop) => {
+  try {
+    return errors.mapped()[prop].msg;
+  } catch (err) {
+    return "";
+  }
+};
+
+module.exports = ({ req, errors }) => {
   return layout({
     content: `<p>Your ID is:${req.session.userID}</p>
   <div>
     <form method="POST">
       <input name="email" placeholder="email"></input>
+      ${getErrors(errors, "email")}
       <input name="password" placeholder="password"></input>
+      ${getErrors(errors, "password")}
       <input name="passwordConfirmation" placeholder="password confirm"></input>
+      ${getErrors(errors, "passwordConfirmation")}
       <button>Sign Up</button>
     </form>
   </div>`
