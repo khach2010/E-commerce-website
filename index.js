2abe040de5640edac5317100af869b9da4425c6c
khach2010 <41897918+khach2010@users.noreply.github.com>
2020-04-29 19:11:59 +0200

    create crat and update product quantity to cart

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
-    keys: ["iloveyou"]
+    keys: ["nogain"]
   })
 );
 
--- routes/cart.js
+++ routes/cart.js
@@ -1,10 +1,35 @@
 const express = require("express");
+const cartsRepo = require("../repository/carts");
 
 const router = express.Router();
 
 //Receive a POST request to add aan item to cart
-router.post("/cart/products", (req, res) => {
-  console.log(req.body.productID);
+router.post("/cart/products", async (req, res) => {
+  // Figure out the card
+  let cart;
+  if (!req.session.cartId) {
+    //we dont have a cart, we need to create one,
+    //and store the cart id to the req.session.cartID
+    //property
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    // we have a cart! Let get it from the repository
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+
+  const existingItem = cart.items.find(item => item.id === req.body.productID);
+  if (existingItem) {
+    // increment quantity and save cart
+    existingItem.quantity++;
+  } else {
+    // add new product id to items array
+    cart.items.push({ id: req.body.productID, quantity: 1 });
+  }
+
+  await cartsRepo.update(cart.id, {
+    items: cart.items
+  });
 
   res.send("Product added to cart");
 });
